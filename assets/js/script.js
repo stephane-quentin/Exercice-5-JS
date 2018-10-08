65245d416e916b0bf14e983fc8967f74300a0054
Stiouf <stephane.quentin@live.fr>
2018-10-08 12:00:47 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,13 @@ function ex5(){
   var first = Math.trunc(document.getElementById("firstNumber").value); //ne garde que la paartie entière du chiffre récupéré dans la variable firstNumber
   var second = document.getElementById("secondNumber").value;
 
-  if(isNaN(first) || isNaN(second)){ // OU logique 
-    alert('Erreur!');
-    } else {
-      alert("Résultat : " + (first * second)); // fait la multiplication entre le contenu de la variable first et celui de la variable second
-}
+  if(first=='' || second==''){
+    alert('Au moins un champ est vide')
+    }else{
+      if(isNaN(first) || isNaN(second)){ // OU logique
+        alert('Erreur!');
+      } else {
+        alert("Résultat : " + (first * second)); // fait la multiplication entre le contenu de la variable first et celui de la variable second
+      }
+    }
 }
