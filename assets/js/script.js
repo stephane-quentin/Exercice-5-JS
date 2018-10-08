f0de9afd0072593d92b126b1eadf569b7368df1a
Stiouf <stephane.quentin@live.fr>
2018-10-08 11:24:34 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 function ex5(){
   var first = Math.trunc(document.getElementById("firstNumber").value); //ne garde que la paartie entière du chiffre récupéré dans la variable firstNumber
   var second = document.getElementById("secondNumber").value;
-  alert("Résultat : " + (first * second)); // fait la multiplication entre le contenu de la variable first et celui de la variable second
+
+  if(isNaN(first) || isNaN(second)){ // OU logique 
+    alert('Erreur!');
+    } else {
+      alert("Résultat : " + (first * second)); // fait la multiplication entre le contenu de la variable first et celui de la variable second
+}
 }
